9587f716e4966c269629c9349b8fc0f97072a916
Carrington <hartis.c99@gmail.com>
2022-12-20 15:05:49 -0500

    card displays with js

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const navbar = document.getElementById('navbar');
-const sticky = navbar.offsetTop;
+if (window.location.pathname == '/index.html') {
+  const navbar = document.getElementById('navbar');
+  const sticky = navbar.offsetTop;
 
-function addStickyClass() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky')
-  } else {
-    navbar.classList.remove('sticky');
+  function addStickyClass() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add('sticky')
+    } else {
+      navbar.classList.remove('sticky');
+    }
   }
+
+  window.onscroll = function () {
+    addStickyClass()
+  };
 }
 
-window.onscroll = function () {
-  addStickyClass()
-};
+const cardArray = [
+  {
+    name: 'Fool',
+    img: 'images/major-arcana/the-fool.jpg'
+  },
+  {
+    name: 'Magician',
+    img: 'images/major-arcana/the-magician.jpg'
+  }
+]
+
+const container = document.querySelector('.container')
+const card = document.createElement('img')
+card.setAttribute('src', 'images/major-arcana/the-fool.jpg')
+container.appendChild(card)
+
+console.log(cardArray[0].name);
+
