3c5287fbfbd205a2b9aced3774234bd76bfce02b
Carrington <hartis.c99@gmail.com>
2022-12-22 17:57:13 -0500

    card array

--- script.js
+++ script.js
@@ -17,13 +17,93 @@ if (window.location.pathname == '/') {
 
 const cardArray = [
   {
-    name: 'Fool',
+    name: 'fool',
     img: 'images/major-arcana/the-fool.jpg'
   },
   {
-    name: 'Magician',
+    name: 'magician',
     img: 'images/major-arcana/the-magician.jpg'
-  }
+  },
+  {
+    name: 'high priestess',
+    img: 'images/major-arcana/the-high-priestess.jpg'
+  },
+  {
+    name: 'empress',
+    img: 'images/major-arcana/the-empress.jpg'
+  },
+  {
+    name: 'emperor',
+    img: 'images/major-arcana/the-emperor.jpg'
+  },
+  {
+    name: 'hierophant',
+    img: 'images/major-arcana/the-hierophant.jpg'
+  },
+  {
+    name: 'lovers',
+    img: 'images/major-arcana/the-lovers.jpg'
+  },
+  {
+    name: 'chariot',
+    img: 'images/major-arcana/the-chariot.jpg'
+  },
+  {
+    name: 'strength',
+    img: 'images/major-arcana/the-strength.jpg'
+  },
+  {
+    name: 'hermit',
+    img: 'images/major-arcana/the-hermit.jpg'
+  },
+  {
+    name: 'wheel of fortune',
+    img: 'images/major-arcana/wheel-of-fortune.jpg'
+  },
+  {
+    name: 'justice',
+    img: 'images/major-arcana/justice.jpg'
+  },
+  {
+    name: 'hanged man',
+    img: 'images/major-arcana/the-hanged-man.jpg'
+  },
+  {
+    name: 'death',
+    img: 'images/major-arcana/death.jpg'
+  },
+  {
+    name: 'temperance',
+    img: 'images/major-arcana/temperance.jpg'
+  },
+  {
+    name: 'devil',
+    img: 'images/major-arcana/the-devil.jpg'
+  },
+  {
+    name: 'tower',
+    img: 'images/major-arcana/the-tower.jpg'
+  },
+  {
+    name: 'star',
+    img: 'images/major-arcana/the-star.jpg'
+  },
+  {
+    name: 'moon',
+    img: 'images/major-arcana/the-moon.jpg'
+  },
+  {
+    name: 'sun',
+    img: 'images/major-arcana/the-sun.jpg'
+  },
+  {
+    name: 'judgement',
+    img: 'images/major-arcana/judgement.jpg'
+  },
+  {
+    name: 'world',
+    img: 'images/major-arcana/the-world.jpg'
+  },
 ]
 
 if (window.location.pathname == '/test.html') {
