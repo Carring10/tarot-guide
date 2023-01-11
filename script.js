1a6d23c92379c79b5dcc885ddcfb0b9da89bac2f
Carrington <hartis.c99@gmail.com>
2023-01-11 17:17:20 -0500

    converted for each to for loop and each card gets a specific meaning

--- script.js
+++ script.js
@@ -19,112 +19,156 @@ const cardArray = [
   {
     name: 'The Fool',
     img: 'images/major-arcana/the-fool.jpg',
-    description: 'It appears your path is about to change, let go of any baggage weighing you down and embrace opportunities with optimism. Free yourself of any worries and doubts, now is the time to be spontanious without a care.'
+    situation: 'It appears your path is about to change, let go of any baggage weighing you down and embrace opportunities with optimism. Free yourself of any worries and doubts, now is the time to be spontanious without a care.',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'The Magician',
     img: 'images/major-arcana/the-magician.jpg',
-    description: "Don't underestimate your talents! You have the skills to accomplish your goals at this time, take advantage of your abilities and continue with confidence."
+    situation: "Don't underestimate your talents! You have the skills to accomplish your goals at this time, take advantage of your abilities and continue with confidence.",
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'The High Priestess',
     img: 'images/major-arcana/the-high-priestess.jpg',
-    description: "Trust your instincts before anything else. No need to seek advice from outside sources, you already have the knowledge and wisdom within to guide yourself."
+    situation: "Trust your instincts before anything else. No need to seek advice from outside sources, you already have the knowledge and wisdom within to guide yourself.",
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'The Empress',
     img: 'images/major-arcana/the-empress.jpg',
-    description: 'This is an indicator that you are entering a period of creativity. During this time, harness your inspiration to bring your ideas to fruition.'
+    situation: 'This is an indicator that you are entering a period of creativity. During this time, harness your inspiration to bring your ideas to fruition.',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'The Emperor',
     img: 'images/major-arcana/the-emperor.jpg',
-    description: 'Take this as a sign to work towards stability by practicing hard work and discipline to bring you closer to reaching your goals. You will benefit from consulting an authority figure in your life, either a parent or boss could act as a mentor.'
+    situation: 'Take this as a sign to work towards stability by practicing hard work and discipline to bring you closer to reaching your goals. You will benefit from consulting an authority figure in your life, either a parent or boss could act as a mentor.',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'The Hierophant',
     img: 'images/major-arcana/the-hierophant.jpg',
-    description: 'If you are considering challenging the status quo, now is not the time to shake things up. Focus on tried and true methods for your future endeavors.'
+    situation: 'If you are considering challenging the status quo, now is not the time to shake things up. Focus on tried and true methods for your future endeavors.',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'The Lovers',
     img: 'images/major-arcana/the-lovers.jpg',
-    description: 'Looks like you have love in the forecast. This can indicate a relationship, and though it is usually romantic in nature, it could be any sort of relationship where good communication is involved.'
+    situation: 'Looks like you have love in the forecast. This can indicate a relationship, and though it is usually romantic in nature, it could be any sort of relationship where good communication is involved.',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'The Chariot',
     img: 'images/major-arcana/the-chariot.jpg',
-    description: 'It appears your current undertakings are progressing. Conquer the upcoming challenges with confidence, you got this.'
+    situation: 'It appears your current undertakings are progressing. Conquer the upcoming challenges with confidence, you got this.',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'Strength',
     img: 'images/major-arcana/strength.jpg',
-    description: "No matter what you are facing right now, know that you have the power to transcend these obstacles. You are resilient, and your willpower will lead to positive results. You may be tested now but don't lose hope!"
+    situation: "No matter what you are facing right now, know that you have the power to transcend these obstacles. You are resilient, and your willpower will lead to positive results. You may be tested now but don't lose hope!",
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'hermit',
     img: 'images/major-arcana/the-hermit.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'wheel of fortune',
     img: 'images/major-arcana/wheel-of-fortune.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'justice',
     img: 'images/major-arcana/justice.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'hanged man',
     img: 'images/major-arcana/the-hanged-man.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'death',
     img: 'images/major-arcana/death.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'temperance',
     img: 'images/major-arcana/temperance.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'devil',
     img: 'images/major-arcana/the-devil.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'tower',
     img: 'images/major-arcana/the-tower.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'star',
     img: 'images/major-arcana/the-star.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'moon',
     img: 'images/major-arcana/the-moon.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'sun',
     img: 'images/major-arcana/the-sun.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'judgement',
     img: 'images/major-arcana/judgement.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   },
   {
     name: 'world',
     img: 'images/major-arcana/the-world.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+    situation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    obstacle: "obstable reading",
+    advice: "advice reading"
   }
 ]
 
@@ -136,11 +180,10 @@ const shuffledCards = [...cardArray].sort(() => 0.5 - Math.random());
 const chosenCards = [];
 
 function createCards() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 3; i++) {
     const card = document.createElement('img');
 
     card.setAttribute('src', 'images/card-back.png');
-    card.setAttribute('id', 'card-img');
     container.appendChild(card);
     card.style.height = '400px';
     // reveal card
@@ -157,18 +200,31 @@ function createCards() {
 }
 
 function revealMeaning() {
-  if (chosenCards.length === 5) {
-    chosenCards.forEach(card => {
+  if (chosenCards.length === 3) {
+    for (let i = 0; i < chosenCards.length; i++) {
       const img = document.createElement('img');
       const p = document.createElement('p');
-      const text = document.createTextNode(card.description);
-
-      img.setAttribute('src', card.img);
+      const text = document.createTextNode(chosenCards.description);
+      
+      img.setAttribute('src', chosenCards[i].img);
+      img.setAttribute('id', i);
       img.style.height = '400px';
       descriptions.appendChild(img);
-      descriptions.appendChild(p);
-      p.appendChild(text);
-    })
+      
+      if (img.id == 0) {
+        const text = document.createTextNode(chosenCards[i].situation);
+        descriptions.appendChild(p);
+        p.appendChild(text);
+      } else if (img.id == 1) {
+        const text = document.createTextNode(chosenCards[i].advice);
+        descriptions.appendChild(p);
+        p.appendChild(text);
+      } else {
+        const text = document.createTextNode(chosenCards[i].obstacle);
+        descriptions.appendChild(p);
+        p.appendChild(text);
+      }
+    }
     fadeOutCards();
     // fadeInMeaning();
   }
